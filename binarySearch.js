652fba546ab592d284f4b1a33d6d456a20d6ad59
larocquedylan <dylancblarocque@gmail.com>
2024-01-15 19:16:02 -0800

    refactor binarySearch

--- binarySearch.js
+++ binarySearch.js
@@ -25,17 +25,42 @@ binarySearch([
 
 export function binarySearch(arr, input, start = 0, end = arr.length - 1) {
     if (start > end) {
-        return -1; // Element not found
+        return -1; 
     }
 
     const mid = Math.floor((start + end) / 2);
 
     if (arr[mid] === input) {
-        return mid; // Element found
+        return mid; 
     } else if (arr[mid] > input) {
-        return binarySearch(arr, input, start, mid - 1); // Search in the left half
+        return binarySearch(arr, input, start, mid - 1); 
     } else {
-        return binarySearch(arr, input, mid + 1, end); // Search in the right half
+        return binarySearch(arr, input, mid + 1, end); 
     }
 }
 
+export function refactorBinarySearch(arr, element){
+  let start = 0; // the zero index
+  let end = arr.length - 1;
+  let mid = Math.floor((start + end) / 2);
+
+  // while mid != change the recalculate the mid
+  // keep in the while look until the start is 
+  // is less than or equal to the end so it will
+  // return -1 if start <= end
+  while (element !== arr[mid]){
+    if(element < arr[mid]){
+      end = mid -1;
+    } else {
+      start = mid + 1;
+    }
+    mid = Math.floor(start + end / 2)
+    // if the mid == to the element return the elemenet
+    if (element === arr[mid]){
+      return element
+    } return -1; // if it isn't after the while loop return -1
+  }
+}
+
+const x = refactorBinarySearch([1,2,3], 4);
+console.log(x);
